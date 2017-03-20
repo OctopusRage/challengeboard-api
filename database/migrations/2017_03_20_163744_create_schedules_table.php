6a01ae65d149fe877ec8d50605fd145b8f89e76d
octopus <ahyana1995@gmail.com>
2017-03-20 12:43:34 -0400

    add create schedules_table migrations

---
+++ database/migrations/2017_03_20_163744_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('event_date');
+            $table->time('event_time');
+            $table->text('places')->nullable();
+            $table->boolean('is_online')->default(false);
+            $table->integer('challenge_id');
+            $table->timestamps();
+            $table->index('challenge_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
