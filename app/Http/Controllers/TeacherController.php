c865b6df13585857a1d0afc9c4fe5ea7ee81ecfc
octopus <ahyana1995@gmail.com>
2017-03-20 11:53:43 -0400

    fix pending request query

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -16,7 +16,7 @@ class TeacherController extends Controller
   }
 
   public function pending_request() {
-    $challenge = Challenge::all($id);
+    $challenge = Challenge::all();
     $current_user = Auth::user();
     if($current_user->isStudent()) {
       return response()->json([
@@ -26,19 +26,18 @@ class TeacherController extends Controller
         ]
       ], 401);
     }
-    
-    $teacher = $challenge->teachers->find($current_user->id);
-    if(empty($teacher)) {
-      return response()->json([
-        'status' => 'fail',
-        'data' => [
-          'messages' => 'unauthorized access'
-        ]
-      ], 401);
-    }
 
-    $list_of_pending = ChallengesParticipant::where('user_id')
-    $pending_request = $challenge->participants->where('status', '=',0);
+    $pending_request = Challenge::join('challenges_participants', 'challenges_participants.challenge_id', '=', 'challenges.id')
+      ->join('challenges_teachers', 'challenges_teachers.challenge_id', '=', 'challenges.id' )
+      ->join('users', 'challenges_participants.user_id', '=', 'users.id')
+      ->join('roles', 'users.role_id', '=', 'roles.id')
+      ->where('roles.role', '=', 'student')
+      ->where('challenges_teachers.user_id', '=', $current_user->id)
+      ->where('challenges_participants.status', '=', false)
+      ->select('challenges_participants.id', 'users.id as user_id', 'users.name', 'challenges_participants.status',
+        'challenges.id as challenge_id', 'challenges.title'
+      )
+      ->get();
     return response()->json([
       'status' => 'success', 
       'data' => [
